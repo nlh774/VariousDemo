08b5e1f157e718b09b42b6863522ba909cef5110
钮林华 <nlh07230@17u.cn>
2015-11-04 17:48:43 +0800

    第一次首次提交，主要是EF6（目前的最新稳定版，EF7处于预发布测试阶段）使用项目

---
+++ EF6Model/AccountContract.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace EF6Model
+{
+    [Table("TCBaseAccount")]
+    public class AccountContract
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        [Key]
+        [Column("AID")]
+        public long AccountId { get; set; }
+
+        /// <summary>
+        /// 账号标识
+        /// </summary>
+        [Column("aKey")]
+        [StringLength(256)]
+        public string Key { get; set; }
+
+        /// <summary>
+        /// 账号所属的消息集合
+        /// </summary>
+        public virtual ICollection<MessageContract> Messages { get; set; }
+    }
+}
---
+++ EF6Model/EFTestContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace EF6Model
+{
+    public class EFTestContext : DbContext
+    {
+        public DbSet<MessageContract> Message { get; set; }
+
+        public DbSet<AccountContract> Account { get; set; }
+
+        public EFTestContext()
+            : base("server=10.1.25.17;uid=sa;pwd=tc004532;database=EFTest")
+        {
+            //第一次运行需要建立DB架构，并插入测试数据。之后可删除
+            //Database.CreateIfNotExists();
+            //this.Account.Add(new AccountContract() {Key = "testAcount"});
+            //this.Message.Add(new MessageContract() {Title = "title1", Content = "content1", AccountId = 1});
+            //this.Message.Add(new MessageContract() { Title = "title2", Content = "content2", AccountId = 1 });
+            //this.SaveChanges();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //账号-消息，一对多关系
+            modelBuilder.Entity<AccountContract>()
+                .HasMany(t => t.Messages).WithRequired(t=>t.Account);
+        }
+    }
+}
